4d8b5489982cb88227cedb91e3b4f7eed8d10706
Kevin Chapman <kevinqchapman@gmail.com>
2022-05-03 15:05:20 +0200

    TabBar for questPage.dart & challengePage.dart

--- lib/questPage.dart
+++ lib/questPage.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
 
-class QuestPage extends StatelessWidget {
-  const QuestPage({Key? key}) : super(key: key);
+class QuestPage extends StatefulWidget {
+   QuestPage({Key? key}) : super(key: key);
+  State<QuestPage> createState() => _MyTabbedPageState();
+
+}
+
+class _MyTabbedPageState extends State<QuestPage> with SingleTickerProviderStateMixin {
+  static const List<Tab> myTabs = <Tab>[
+    Tab(text: 'LEFT'),
+    Tab(text: 'RIGHT'),
+  ];
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: myTabs.length);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('This is the center of the Quest page')),
+    return Scaffold(
+      appBar: AppBar(
+        title: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(text: 'Discover'),
+            Tab(text: 'Completed'),
+          ],
+        ),
+      ),
     );
   }
 }
