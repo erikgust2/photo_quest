35022f1f05d32c8597bc58fe24f6a02eb8860601
OscarHansson72 <oscar_oh_94@hotmail.com>
2022-05-12 15:59:34 +0200

    added GoogleSignIn.dart added android/google-services.json
    app/build.gradle:
    * apply plugin: 'com.google.gms.google-services'
    * dependencies{}
    
    build.gradle:
    * classpath 'com.google.gms:google-services:4.3.10'
    
    pubspec.yaml:
    * provider: ^5.0.0
    * firebase_core: ^1.3.0
    * cloud_firestore: ^2.2.2
    * font_awesome_flutter: ^9.0.0

---
+++ lib/GoogleSignIn.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:photo_quest/GoogleSignInProvider.dart';
+import 'package:provider/provider.dart';
+
+class LoginWidget extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton.icon(
+      style: ElevatedButton.styleFrom(
+        primary: Colors.white,
+        onPrimary: Colors.black,
+        minimumSize: Size(double.infinity, 50),
+      ),
+      icon: FaIcon(FontAwesomeIcons.google),
+      label: Text('Log in with Google'),
+      onPressed: () {
+        final provider = Provider.of<GoogleSignInProvider>(context, listen: false);
+        provider.googleLogin();
+        print(FirebaseAuth.instance.currentUser?.email);
+      },
+    );
+
+  }
+
+
+}
