211477053f6ce4cde60a136e7a43f9179a4fff2d
Kevin Chapman <kevinqchapman@gmail.com>
2022-05-25 17:14:03 +0200

    Fixed questAvailable ListView bug MAYBE

--- lib/quest_map.dart
+++ lib/quest_map.dart
@@ -81,8 +81,7 @@ class _QuestMapScreenState extends State<QuestMapScreen> {
             )));
     setState(() {
       _markers = markers;
-      _loadedItems.clear(); //Either of these could be redundant or useless
-      QuestController().loadedQuests.clear(); //Could be redundant or useless
+      _loadedItems.clear(); //
     });
   }
 
